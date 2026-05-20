1e8914fd72907ab5a08b0147bc0d2df63e5747cd
Ruben van der Linde <rubenvdlinde@gmail.com>
2026-05-20 14:58:16 +0200

    feat(or-cutover): shrink ConsumersController to action methods only

--- lib/Controller/ConsumersController.php
+++ lib/Controller/ConsumersController.php
@@ -2,17 +2,12 @@
 
 namespace OCA\OpenConnector\Controller;
 
-use OCA\OpenConnector\Service\ObjectService;
-use OCA\OpenConnector\Service\SearchService;
-use OCA\OpenConnector\Db\Consumer;
-use OCA\OpenConnector\Db\ConsumerMapper;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Http\JSONResponse;
 use OCP\IAppConfig;
 use OCP\IL10N;
 use OCP\IRequest;
-use OCP\AppFramework\Db\DoesNotExistException;
 
 /**
  * @SuppressWarnings(PHPMD.ShortVariable)
@@ -24,17 +19,15 @@ class ConsumersController extends Controller
     /**
      * Constructor for the ConsumerController
      *
-     * @param string         $appName        The name of the app
-     * @param IRequest       $request        The request object
-     * @param IAppConfig     $config         The app configuration object
-     * @param ConsumerMapper $consumerMapper The consumer mapper object
-     * @param IL10N          $l              The localization service
+     * @param string     $appName The name of the app
+     * @param IRequest   $request The request object
+     * @param IAppConfig $config  The app configuration object
+     * @param IL10N      $l       The localization service
      */
     public function __construct(
         $appName,
         IRequest $request,
         private IAppConfig $config,
-        private ConsumerMapper $consumerMapper,
         private IL10N $l
     ) {
         parent::__construct($appName, $request);
@@ -58,125 +51,4 @@ class ConsumersController extends Controller
             []
         );
     }//end page()
-
-    /**
-     * Retrieves a list of all consumers
-     *
-     * This method returns a JSON response containing an array of all consumers in the system.
-     *
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @return JSONResponse A JSON response containing the list of consumers
-     */
-    public function index(ObjectService $objectService, SearchService $searchService): JSONResponse
-    {
-        $filters        = $this->request->getParams();
-        $fieldsToSearch = ['name', 'description'];
-
-        $searchParams     = $searchService->createMySQLSearchParams(filters: $filters);
-        $searchConditions = $searchService->createMySQLSearchConditions(filters: $filters, fieldsToSearch:  $fieldsToSearch);
-        $filters          = $searchService->unsetSpecialQueryParams(filters: $filters);
-
-        return new JSONResponse(['results' => $this->consumerMapper->findAll(limit: null, offset: null, filters: $filters, searchConditions: $searchConditions, searchParams: $searchParams)]);
-    }//end index()
-
-    /**
-     * Retrieves a single consumer by its ID
-     *
-     * This method returns a JSON response containing the details of a specific consumer.
-     *
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @param  string $id The ID of the consumer to retrieve
-     * @return JSONResponse A JSON response containing the consumer details
-     */
-    public function show(string $id): JSONResponse
-    {
-        try {
-            return new JSONResponse($this->consumerMapper->find(id: (int) $id));
-        } catch (DoesNotExistException $exception) {
-            return new JSONResponse(data: ['error' => $this->l->t('Not Found')], statusCode: 404);
-        }
-    }//end show()
-
-    /**
-     * Creates a new consumer
-     *
-     * This method creates a new consumer based on POST data.
-     *
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @return JSONResponse A JSON response containing the created consumer
-     */
-    public function create(): JSONResponse
-    {
-        $data = $this->request->getParams();
-
-        foreach ($data as $key => $value) {
-            if (str_starts_with($key, '_')) {
-                unset($data[$key]);
-            }
-        }
-
-        if (isset($data['id'])) {
-            unset($data['id']);
-        }
-
-        // Create the consumer
-        $consumer = $this->consumerMapper->createFromArray(object: $data);
-
-        return new JSONResponse($consumer);
-    }//end create()
-
-    /**
-     * Updates an existing consumer
-     *
-     * This method updates an existing consumer based on its ID.
-     *
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @param  string $id The ID of the consumer to update
-     * @return JSONResponse A JSON response containing the updated consumer details
-     */
-    public function update(int $id): JSONResponse
-    {
-        $data = $this->request->getParams();
-
-        foreach ($data as $key => $value) {
-            if (str_starts_with($key, '_')) {
-                unset($data[$key]);
-            }
-        }
-
-        if (isset($data['id'])) {
-            unset($data['id']);
-        }
-
-        // Update the consumer
-        $consumer = $this->consumerMapper->updateFromArray(id: (int) $id, object: $data);
-
-        return new JSONResponse($consumer);
-    }//end update()
-
-    /**
-     * Deletes a consumer
-     *
-     * This method deletes a consumer based on its ID.
-     *
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @param  string $id The ID of the consumer to delete
-     * @return JSONResponse An empty JSON response
-     */
-    public function destroy(int $id): JSONResponse
-    {
-        $this->consumerMapper->delete($this->consumerMapper->find((int) $id));
-
-        return new JSONResponse([]);
-    }//end destroy()
 }//end class
