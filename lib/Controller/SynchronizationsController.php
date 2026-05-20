6a2599bddc5d0341f19180c345c07f70ef862e26
Ruben van der Linde <rubenvdlinde@gmail.com>
2026-05-20 14:58:35 +0200

    feat(or-cutover): shrink SynchronizationsController to action methods only

--- lib/Controller/SynchronizationsController.php
+++ lib/Controller/SynchronizationsController.php
@@ -3,7 +3,6 @@
 namespace OCA\OpenConnector\Controller;
 
 use GuzzleHttp\Exception\GuzzleException;
-use OCA\OpenConnector\Service\ObjectService;
 use OCA\OpenConnector\Service\SearchService;
 use OCA\OpenConnector\Service\SynchronizationService;
 use OCA\OpenConnector\Db\SynchronizationMapper;
@@ -39,9 +38,15 @@ class SynchronizationsController extends Controller
     /**
      * Constructor for the SynchronizationsController
      *
-     * @param string     $appName The name of the app
-     * @param IRequest   $request The request object
-     * @param IAppConfig $config  The app configuration object
+     * @param string                        $appName                        The name of the app
+     * @param IRequest                      $request                        The request object
+     * @param IAppConfig                    $config                         The app configuration object
+     * @param SynchronizationMapper         $synchronizationMapper          The synchronization mapper
+     * @param SynchronizationContractMapper $synchronizationContractMapper  The synchronization contract mapper
+     * @param SynchronizationLogMapper      $synchronizationLogMapper       The synchronization log mapper
+     * @param SynchronizationService        $synchronizationService         The synchronization service
+     * @param IL10N                         $l                              The localization service
+     * @param LoggerInterface               $logger                         The logger
      */
     public function __construct(
         $appName,
@@ -76,121 +81,6 @@ class SynchronizationsController extends Controller
             []
         );
     }//end page()
-
-    /**
-     * Retrieves a list of all synchronizations
-     *
-     * This method returns a JSON response containing an array of all synchronizations in the system.
-     *
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @return JSONResponse A JSON response containing the list of synchronizations
-     */
-    public function index(ObjectService $objectService, SearchService $searchService): JSONResponse
-    {
-        $filters        = $this->request->getParams();
-        $fieldsToSearch = ['name', 'description'];
-
-        $searchParams     = $searchService->createMySQLSearchParams(filters: $filters);
-        $searchConditions = $searchService->createMySQLSearchConditions(filters: $filters, fieldsToSearch:  $fieldsToSearch);
-        $filters          = $searchService->unsetSpecialQueryParams(filters: $filters);
-
-        return new JSONResponse(['results' => $this->synchronizationMapper->findAll(limit: null, offset: null, filters: $filters, searchConditions: $searchConditions, searchParams: $searchParams)]);
-    }//end index()
-
-    /**
-     * Retrieves a single synchronization by its ID
-     *
-     * This method returns a JSON response containing the details of a specific synchronization.
-     *
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @param  string $id The ID of the synchronization to retrieve
-     * @return JSONResponse A JSON response containing the synchronization details
-     */
-    public function show(string $id): JSONResponse
-    {
-        try {
-            return new JSONResponse($this->synchronizationMapper->find(id: (int) $id));
-        } catch (DoesNotExistException $exception) {
-            return new JSONResponse(data: ['error' => $this->l->t('Not Found')], statusCode: 404);
-        }
-    }//end show()
-
-    /**
-     * Creates a new synchronization
-     *
-     * This method creates a new synchronization based on POST data.
-     *
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @return JSONResponse A JSON response containing the created synchronization
-     */
-    public function create(): JSONResponse
-    {
-        $data = $this->request->getParams();
-
-        foreach ($data as $key => $value) {
-            if (str_starts_with($key, '_')) {
-                unset($data[$key]);
-            }
-        }
-
-        if (isset($data['id'])) {
-            unset($data['id']);
-        }
-
-        return new JSONResponse($this->synchronizationMapper->createFromArray(object: $data));
-    }//end create()
-
-    /**
-     * Updates an existing synchronization
-     *
-     * This method updates an existing synchronization based on its ID.
-     *
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @param  string $id The ID of the synchronization to update
-     * @return JSONResponse A JSON response containing the updated synchronization details
-     */
-    public function update(int $id): JSONResponse
-    {
-        $data = $this->request->getParams();
-
-        foreach ($data as $key => $value) {
-            if (str_starts_with($key, '_')) {
-                unset($data[$key]);
-            }
-        }
-
-        if (isset($data['id'])) {
-            unset($data['id']);
-        }
-
-        return new JSONResponse($this->synchronizationMapper->updateFromArray(id: (int) $id, object: $data));
-    }//end update()
-
-    /**
-     * Deletes a synchronization
-     *
-     * This method deletes a synchronization based on its ID.
-     *
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @param  string $id The ID of the synchronization to delete
-     * @return JSONResponse An empty JSON response
-     */
-    public function destroy(int $id): JSONResponse
-    {
-        $this->synchronizationMapper->delete($this->synchronizationMapper->find((int) $id));
-
-        return new JSONResponse([]);
-    }//end destroy()
 
     /**
      * Retrieves call logs for a job
