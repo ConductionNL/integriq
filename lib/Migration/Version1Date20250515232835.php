dfa3737132b25e1b4f20e03420abe2d9226213a9
Ruben van der Linde <ruben@conduction.nl>
2026-08-05 20:47:34 +0200

    chore(license): normalise the last AGPL declarations to EUPL-1.2
    The migration's SPDX identifier contradicted the '@license EUPL-1.2' PHPDoc
    tag in the same file. Also corrects openapi.json's info.license.name and the
    two AGPL claims in docs/GOVERNMENT-FEATURES.md.

--- lib/Migration/Version1Date20250515232835.php
+++ lib/Migration/Version1Date20250515232835.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
 
 /*
  * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: AGPL-3.0-or-later
+ * SPDX-License-Identifier: EUPL-1.2
  */
 
 namespace OCA\OpenConnector\Migration;
